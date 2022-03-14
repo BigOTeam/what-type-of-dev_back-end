6597b9d9df73dadea6f8ea1ab353789ce210db4e
황정준 <hsk2454@naver.com>
2022-03-14 13:38:44 +0900

    Feat: spring boot 초기세팅

---
+++ src/test/java/com/bigo/whattypeofdev/WhatTypeOfDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigo.whattypeofdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatTypeOfDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
