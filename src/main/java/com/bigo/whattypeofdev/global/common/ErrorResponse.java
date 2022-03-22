165791143282fa73cecaf8d015f2be9781a16dc7
= <dgh03207@gmail.com>
2022-03-22 10:33:59 +0900

    Feat : Exception Handler 추가

---
+++ src/main/java/com/bigo/whattypeofdev/global/common/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.bigo.whattypeofdev.global.common;
+
+import lombok.Data;
+
+@Data
+public class ErrorResponse {
+    private ErrorStatusEnum status;
+    private String message;
+    private Object data;
+
+    public ErrorResponse(){
+        this.status = null;
+        this.data = null;
+        this.message = null;
+    }
+
+}
---
+++ src/main/java/com/bigo/whattypeofdev/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.bigo.whattypeofdev.global.exception;
+
+import com.bigo.whattypeofdev.global.common.ErrorResponse;
+import com.bigo.whattypeofdev.global.common.ErrorStatusEnum;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity methodArgumentNotValid(MethodArgumentNotValidException e) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatus(ErrorStatusEnum.BAD_REQUEST);
+        errorResponse.setMessage(e.getClass().getName());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = NoHandlerFoundException.class)
+    public ResponseEntity noHandlerFoundException(NoHandlerFoundException e) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatus(ErrorStatusEnum.BAD_REQUEST);
+        errorResponse.setMessage(e.getClass().getName());
+        errorResponse.setData("핸들러 없음");
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(value = CustomException.class)
+    public ResponseEntity customException(CustomException e) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        ErrorStatusEnum errorStatusEnum = e.getErrorStatusEnum();
+        errorResponse.setStatus(errorStatusEnum);
+        errorResponse.setMessage(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorStatusEnum.getCode()));
+    }
+
+}
