a0c4d6b1232319bd86e7b9daefa6e904f14dade5
= <dgh03207@gmail.com>
2022-04-06 21:26:31 +0900

    Modify : @NoArgsConstructor(AccessLevel.PROTECTED) 추가

--- src/main/java/com/bigo/whattypeofdev/domain/surveys/dto/JobListResponseDto.java
+++ src/main/java/com/bigo/whattypeofdev/domain/surveys/dto/JobListResponseDto.java
@@ -2,13 +2,16 @@ package com.bigo.whattypeofdev.domain.surveys.dto;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @ApiModel
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
 public class JobListResponseDto {
     @ApiModelProperty(name="직업 목록")
     private List<JobDto> jobList;
