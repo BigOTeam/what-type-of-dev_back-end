1fba5832e2db80e06a91e8b497deb4732c2bd6af
AYeong Lee <58917737+dgh03207@users.noreply.github.com>
2022-04-07 14:50:17 +0900

    Revert "Merge : statistics 도메인 내 차트타입에 따른 정렬기능 추가"

--- src/main/java/com/bigo/whattypeofdev/domain/statistics/repository/SurveyRecordCustumRepository.java
+++ src/main/java/com/bigo/whattypeofdev/domain/statistics/repository/SurveyRecordCustumRepository.java
@@ -7,10 +7,7 @@ public interface SurveyRecordCustumRepository {
     List<Integer> findAllTopcountByColumn(String column);
     List<String> findAllTopByColumn(String column);
     String findTopByColumn(String column);
-    int countAll(String column);
-    List<Integer> findAllcountByColumnWithFilter(String column, int gender, int age);
-    List<String> findAllByColumnWithFilter(String column, int gender, int age);
-    List<String> findAllByColumn(String column);
-    List<Integer> findAllCountByColumn(String column);
+    int countByColumn(String column);
+    List<Integer> findAllTopcountByColumnWithFilter(String column,int gender,int age);
+    List<String> findAllTopByColumnWithFilter(String column,int gender,int age);
 }
-
